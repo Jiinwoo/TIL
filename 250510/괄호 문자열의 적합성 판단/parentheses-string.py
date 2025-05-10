feed821d2f10dafea80f988035de66ea1d0a29cb
JinWoo Jung <46522752+Jiinwoo@users.noreply.github.com>
2025-05-10 21:32:10 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 83ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 250510/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -0,0 +1,18 @@
+str = input()
+
+# Please write your code here.
+s = []
+
+for c in str:
+    if c == "(":
+        s.append(c)
+    else:
+        if not s:
+            print(No)
+            exit(0)
+        if s[-1] == "(":
+            s.pop()
+if s:
+    print("No")
+else:
+    print("Yes")
