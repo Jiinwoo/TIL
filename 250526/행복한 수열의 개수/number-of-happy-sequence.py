2ceb33aefb3050143cb251f2d7ea056437307e36
JinWoo Jung <46522752+Jiinwoo@users.noreply.github.com>
2025-05-26 22:21:24 +0900

    Title: 행복한 수열의 개수, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

--- 250526/행복한 수열의 개수/number-of-happy-sequence.py
+++ 250526/행복한 수열의 개수/number-of-happy-sequence.py
@@ -4,31 +4,39 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 # Please write your code here.
 
 result = 0
+
 for i in range(n):
     v = grid[i][0]
     c = 1
+    found = False
     for j in range(1, n):
         if v == grid[i][j]:
             c += 1
         else:
             v = grid[i][j]
             c = 1
-
-    if c >= m:
+        if c >= m:
+            found = True
+            break;
+    if found or (c >= m ):
         result += 1
 
 
 for i in range(n):
     v = grid[0][i]
     c = 1
+    found = False
+    
     for j in range(1, n):
         if v == grid[j][i]:
             c += 1
         else:
             v = grid[j][i]
             c = 1
-
-    if c >= m:
+        if c >= m:
+            found = True
+            break;
+    if found or (c >= m ):
         result += 1
 
 print(result)
